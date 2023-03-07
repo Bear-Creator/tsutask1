257e35fa0bad1598e785d365550cb79f75c373b9
Bear-Creator <96181156+Bear-Creator@users.noreply.github.com>
2023-03-07 17:33:44 +0700

    Заменил For на while

--- 7. Fabinachi.cpp
+++ 7. Fabinachi.cpp
@@ -6,29 +6,29 @@ using namespace std;
 int main() {
     double x, func;
     double step = (3.99) / 9.0; 
-    x=0.01;
-    for (int i = 0; i <= 9; i++) {
+    x = 0.01;
+    int i = 0;
+    while (i <= 9) {
         x += step;
         func = sin(x) / x;
         cout << "x = " << x << ", function = " << func << endl;
+        i++;
     }
 
-    cout<< '\n';
+    cout << '\n';
 
     int MAX, n = 3;
-    double summ = 2, n1 = 1, n2 = 1,g;
+    int summ = 2, n1 = 1, n2 = 1, g;
     cout << "Enter a large number MAX: ";
     cin >> MAX;
     while (summ <= MAX) {
-        g=n1;
+        g = n1;
         n1 += n2;
-        n2=g;
-        summ+=n1;
+        n2 = g;
+        summ += n1;
         n++;
     }
 
     cout << "The sum of the first " << n - 1 << " fibonacci numbers is " << summ - n1 << endl;
     cout << "The " << n - 1 << "th Fibonacci number is " << n2 << endl;
-
-
 }
